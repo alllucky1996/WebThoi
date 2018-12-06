e72d3df64107bbad173bb6550816e168f3ecebfa
hongcam <hongcam941997@gmail.com>
2018-12-06 23:38:32 +0700

    Add model National, Unit  and fix user

--- Dung/Model/dmKPI.cs
+++ Dung/Model/dmKPI.cs
@@ -12,41 +12,44 @@ using System.Threading.Tasks;
 namespace Dung.Model
 {
   //  [Table("dmKPI")]
-    [DropDown(ValueField = "Id", TextField = "Name")]
-    public class dmKPI : Dung
+  [DropDown(ValueField = "Id", TextField = "Name")]
+  public class dmKPI : Dung
+  {
+    [Key]
+    public long Id { get; set; }
+    [Display(Name ="Mã")]
+    public string Code { get; set; }
+    #region const
+    public DateTime CreateDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+    public bool IsDeleted { get; set; }
+    [Display(Name="Tên")]
+    public string Name { get; set; }
+    #endregion
+
+    [Display(Name = "Trọng số (%)")]
+    public int TrongSo { get; set; }
+
+    [Display(Name = "Trọng số chỉ tiêu (%)")]
+    public int? TrongSoChiTieu { get; set; }
+
+    public long? IdCha { get; set; }
+    [ForeignKey("IdCha")]
+    public virtual dmKPI DmKPI { get; set; }
+    public long? IdDonVi { get; set; }
+    [ForeignKey("IdDonVi")]
+    public virtual dmDonVi DonVi { get; set; }
+
+		[ForeignKey("Id")]
+		public virtual unit Unit { get; set; }
+
+		public dmKPI()
     {
-        [Key]
-        public long Id { get; set; }
-        [Display(Name ="Mã")]
-        public string Code { get; set; }
-        #region const
-        public DateTime CreateDate { get; set; }
-        public DateTime? UpdateDate { get; set; }
-        public bool IsDeleted { get; set; }
-        [Display(Name="Tên")]
-        public string Name { get; set; }
-        #endregion
-
-        [Display(Name = "Trọng số (%)")]
-        public int TrongSo { get; set; }
-
-        [Display(Name = "Trọng số chỉ tiêu (%)")]
-        public int? TrongSoChiTieu { get; set; }
-
-        public long? IdCha { get; set; }
-        [ForeignKey("IdCha")]
-        public virtual dmKPI DmKPI { get; set; }
-        public long? IdDonVi { get; set; }
-        [ForeignKey("IdDonVi")]
-        public virtual dmDonVi DonVi { get; set; }
-        public dmKPI()
-        {
-            CreateDate = DateTime.Now;
-        }
-        public string Describe()
-        {
-            return "{id: " + Id + "}";
-
-        }
+      CreateDate = DateTime.Now;
     }
+    public string Describe()
+    {
+      return "{id: " + Id + "}";
+    }
+  }
 }
---
+++ Dung/Model/user.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dung.Model
+{
+	class user : Entities.Models.SystemManage.Account
+	{
+		[ForeignKey("Id")]
+		public virtual national National { get; set; }
+	}
+}
