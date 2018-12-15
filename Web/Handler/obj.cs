b147483074b10996da406755df5ddebbd35208be
hongcam <hongcam941997@gmail.com>
2018-12-15 15:37:22 +0700

    Add data

---
+++ Web/Handler/obj.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Handler
+{
+	public class obj
+	{
+		public string Parent;
+		public string Child;
+		public obj(string parent, string child)
+		{
+			Parent = parent;
+			Child = child;
+		}
+	}
+}
